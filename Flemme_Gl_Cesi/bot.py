50953c3361d4e857940a15925979a154c192b989
DraKkarW2 <68158328+DraKkarW2@users.noreply.github.com>
2024-05-14 00:07:17 +0200

    [update] bot 1.1.0

--- Flemme_Gl_Cesi/bot.py
+++ Flemme_Gl_Cesi/bot.py
@@ -2,79 +2,170 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
+def botGl(email,passwords,time_skip):
+    def log(message):
+        print(f"[LOG] {message}")
 
-options = webdriver.ChromeOptions()
-options.add_argument('--incognito')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--disable-dev-shm-usage')
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option('useAutomationExtension', False)
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
+    options = webdriver.ChromeOptions()
+    options.add_argument('--incognito')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
 
-driver = webdriver.Chrome(options=options)
-driver.maximize_window()
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
 
-driver.get('https://auth.global-exam.com/login')
+    driver.get('https://auth.global-exam.com/login')
 
-try:
+    try:
+        log("Attempting to log in...")
+        username = driver.find_element(By.ID, 'email')
+        password = driver.find_element(By.ID, 'password')
+        button = driver.find_element(By.CLASS_NAME, 'button-solid-primary-big')
 
-    username = driver.find_element(By.ID, 'email')
-    password = driver.find_element(By.ID, 'password')
-    button = driver.find_element(By.CLASS_NAME, 'button-solid-primary-big')
+        username.send_keys(email)
+        password.send_keys(passwords)
+        button.click()
+        log("Login submitted.")
 
-    username.send_keys("email")
-    password.send_keys("password")
-    button.click()
+        log("Clicking CESI account button...")
+        button_cesi_acc = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/main/div/div/div/div/div[1]/div'))
+        )
+        button_cesi_acc.click()
+        log("CESI account button clicked.")
 
-    button_cesi_acc= driver.find_element(By.XPATH, '//*[@id="app"]/main/div/div/div/div/div[1]/div')
-    button_cesi_acc.click()
+        log("Navigating to parcours...")
+        button_parcours = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div/header/div/div[3]/div/ul/li[2]/span/a'))
+        )
+        button_parcours.click()
+        log("Navigated to parcours.")
 
-    button_parcours = driver.find_element(By.XPATH, '//*[@id="app"]/div/header/div/div[3]/div/ul/li[2]/span/a')
-    button_parcours.click()
+        log("Clicking continue button...")
+        button_continue = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div/div/main/div[1]/div/div/div[2]/div[3]/button'))
+        )
+        button_continue.click()
+        log("Continue button clicked.")
 
-    button_continue =  WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div/div/main/div[1]/div/div/div[2]/div[3]/button')))
-    button_continue.click()
+        log("Waiting for activity buttons to be visible...")
+        WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'card-hover')))
+        log("Activity buttons are visible.")
 
-    activités = driver.find_elements(By.CLASS_NAME,'w-15 h-15 relative flex items-center justify-center lg:w-30 lg:h-30 rounded-full bg-listening')
+        activities = driver.find_elements(By.CLASS_NAME, 'card-hover')
+        log(f"Found {len(activities)} activity buttons.")
 
-    '''checkbox = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div/main[2]/div/div[2]/div/div[2]/div/div[3]/label[2]/div')))
-    checkbox.click()
+        for activity in activities:
+            try:
+                activity_text_element = activity.find_element(By.TAG_NAME, 'p')
+                activity_text = activity_text_element.text.strip()
 
+                if activity_text in ["Introduction","Work environments - Offices","Colleagues and working relationships","Present continuous - I am doing","Present continuous - I am doing","Describing procedures (how something works, problems, processes, etc.)"]:
+                    log("Skipping 'Introduction' activity.")
+                    continue
 
-    test_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'button-solid-default-small')))
-    test_button.click()
-   
-    # Récupérer tous les blocs de questions et réponses
-    questions = driver.find_elements(By.CSS_SELECTOR, 'div.w-full')
+                score_element = WebDriverWait(activity, 2).until(
+                    EC.presence_of_element_located((By.XPATH, './/span[contains(@class, "font-bold")]'))
+                )
+                score_text = score_element.text.strip()
 
-    for question in questions:
-        # Récupérer le texte de la question
-        question_text = question.text.strip()
-        print("Question:", question_text)
-        
-        # Trouver les réponses possibles à la question
-        answers = question.find_elements(By.XPATH, '../following-sibling::div//label//div[@class="flex items-center justify-center shrink-0 w-6 h-6 border rounded mr-4"]')
-        
-        # Parcourir chaque réponse possible
-        possible_answers = []
-        for answer in answers:
-            # Récupérer le texte de la réponse
-            answer_text = answer.text.strip()
-            possible_answers.append(answer_text)
-        
-        print("Réponses possibles:", possible_answers)
-        
-        # Répondez aléatoirement à la question (remplacez ceci par votre logique de réponse)
-        # par exemple, choisissez une réponse au hasard
-        # import random
-        # random_answer = random.choice(possible_answers)
-        # print("Réponse choisie:", random_answer)'''
+                if score_text == "100%":
+                    continue
+            except Exception as e:
+                log(f"Exception while checking activity: {e}")
+                activity.click()
+                log("Clicked on an activity.")
+                break
 
-except Exception as e:
-    print("Error:", e)
-finally:
-    # Ajoutez une pause pour voir le résultat avant de fermer le navigateur
-    input("Press Enter to close...")
-    driver.quit()
+        try:
+            start_button = WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located((By.XPATH, '//button[text()="Démarrer"]'))
+            )
+            if start_button.is_displayed():
+                start_button.click()
+                log("Clic 'Démarrer' button.")
+        except Exception as e:
+            log(f"Exception clicking 'Démarrer': {e}")
+
+        while True:
+            try:
+                progress_text = WebDriverWait(driver, 10).until(
+                    EC.visibility_of_element_located((By.XPATH, '//p[contains(@class, "progress-bar-arrow-top")]'))
+                ).text.strip()
+                current_progress, total_progress = map(int, progress_text.split('/'))
+                log(f"Progress: {current_progress}/{total_progress}")
+
+                if current_progress == total_progress:
+                    log("All questions have been answered.")
+                    break
+
+                correction_buttons = WebDriverWait(driver, 10).until(
+                    EC.presence_of_all_elements_located((By.XPATH, '//span[text()="Voir la correction"]'))
+                )
+                for button in correction_buttons:
+                    try:
+                        button.click()
+                        log("Clicked 'Voir la correction' button.")
+                        time.sleep(1)
+                    except Exception as e:
+                        log(f"Error clicking 'Voir la correction' button: {e}")
+
+                labels = WebDriverWait(driver, 10).until(
+                    EC.presence_of_all_elements_located((By.CLASS_NAME, 'bg-success-05'))
+                )
+                for label in labels:
+                    try:
+                        driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", label)
+                        time.sleep(1)
+
+                        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(label))
+                        label.click()
+                        log("Clicked a label with class 'bg-success-05'.")
+                        time.sleep(1)
+                    except Exception as e:
+                        log(f"Error clicking label: {e}")
+
+                time.sleep(2)
+
+                valider_button = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, '//button[contains(@class, "button-solid-primary-large") and text()="Valider"]'))
+                )
+                valider_button.click()
+                log("Clicked 'Valider' button.")
+                time.sleep(5)
+
+            except Exception as e:
+                log(f"Error during process: {e}")
+                break
+
+        log("Process completed. Waiting 30 minutes before clicking 'Terminer'.")
+        time.sleep(time_skip)  # Attendre 30 minutes 30 * 60
+
+        try:
+            terminer_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//button[contains(@class, "button-solid-primary-large") and text()="Terminer"]'))
+            )
+            terminer_button.click()
+            log("Clicked 'Terminer' button.")
+        except Exception as e:
+            log(f"Error clicking 'Terminer' button: {e}")
+
+        log("Waiting to close the browser.")
+        time.sleep(5)
+
+    except Exception as e:
+        log(f"Error: {e}")
+    finally:
+        input("Press Enter to close...")
+        driver.quit()
+
+email = "email"
+passwords = "password"
+time_skip = 1    
+botGl(email,passwords,time_skip)
